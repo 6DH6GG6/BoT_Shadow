d3f466108645a752fa0cd6309aa78ce8c4906020
🔥『𝑴𝒂𝒓𝒔𝒉𝒆𝒍 𝑫𝒆 𝑺𝒉𝒂𝒅𝒐𝒘』🔥 <dalam70000@gmail.com>
2026-03-21 00:52:27 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,17 +9,20 @@ module.exports = {
         // جمع جميع الأوامر الحقيقية فقط
         const allCommands = Array.from(commands.values())
             .map(cmd => cmd.name)
-            .filter(Boolean); // فقط التي لها اسم
+            .filter(Boolean) // فقط التي لها اسم
+            .sort(); // ترتيب أبجدي (اختياري)
 
-        let reply = "👑👑👑👑👑\n";
+        let reply = "༻l مطــوࢪ البـوت l༺\n";
+        reply += "👑  𝑺𝑯𝑨𝑫𝑶𝑾 𝑶𝑮  👑\n\n";
+        reply += "<<<<<<<<<<<\n";
+        reply += "𓊈𓆪 كل الأوامر 𓆪𓊉\n";
+        reply += "<<<<<<<<<<<\n\n";
+
+        reply += "♦♦♦♦♦♦♦\n";
         allCommands.forEach((cmdName, i) => {
             reply += `- ${i + 1}〖/${cmdName}〗\n`;
         });
-        reply += "_____________\n";
-        reply += `كل الأوامر\n`;
-        reply += "_____________\n";
-        reply += "𝑺𝑯𝑨𝑫𝑶𝑾 𝑶𝑮\n";
-        reply += "👑👑👑👑👑";
+        reply += "♦♦♦♦♦♦";
 
         try {
             await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
